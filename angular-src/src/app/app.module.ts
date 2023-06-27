7cccf3a1f35ca0fbe2f81e11166b49b1db488ce5
me mee <me@email.com>
2023-06-27 16:47:26 +0300

    final base for project

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -2,19 +2,58 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProfileComponent } from './components/profile/profile.component';
+
+import {ValidateService} from './services/validate.service';
+import { AuthService } from './services/auth.service';
+import {FlashMessagesModule} from 'angular2-flash-messages';
+import { AuthGuard } from './guards/auth.guard';
+
+//import {MessageService} from './services/message.service';
+//import { ReadComponent } from './components/read/read.component';
+
+const appRoutes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'register', component: RegisterComponent},
+  {path:'login', component: LoginComponent},
+  {path:'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
+  {path:'profile', component: ProfileComponent, canActivate: [AuthGuard]},
+  //{path:'read', component: ReadComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    DashboardComponent,
+    ProfileComponent,
+    //ReadComponent,
+    NavbarComponent,
+    HomeComponent,
+    DashboardComponent,
+    LoginComponent,
+    ProfileComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
+    FlashMessagesModule  
   ],
-  providers: [],
+  providers: [ValidateService, AuthService, AuthGuard, /*MessageService*/],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
